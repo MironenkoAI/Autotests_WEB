9f0b1ce328af14d8eea19df3e1246ad721884ef8
Anastasiya Mironenko <nastya_911@mail.ru>
2023-10-23 20:53:02 +0300

    работа на лекции с Page Object

---
+++ lec_3_Page_Object/test_1.py
@@ -0,0 +1,9 @@
+from testpage import OperationsHelper
+
+def test_step1(browser):
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login("test")
+    testpage.enter_pass("test")
+    testpage.click_login_button()
+    assert testpage.get_error_text() == "401"
