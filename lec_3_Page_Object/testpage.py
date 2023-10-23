5e79da70c66d0fa95ac46ddaa745b15d7f332f55
Anastasiya Mironenko <nastya_911@mail.ru>
2023-10-23 23:35:48 +0300

    работа на лекции с Page Object, логирование logging и pytest.ini

--- lec_3_Page_Object/testpage.py
+++ lec_3_Page_Object/testpage.py
@@ -1,5 +1,6 @@
 from BaseApp import BasePage
 from selenium.webdriver.common.by import By
+import logging
 
 class TestSearchLocators:
     LOCATOR_LOGIN_FIELD = (By.XPATH, """// *[ @ id = "login"] / div[1] / label / input""")
@@ -9,18 +10,23 @@ class TestSearchLocators:
 
 class OperationsHelper(BasePage):
     def enter_login(self, word):
+        logging.info(f"Send {word} to element {TestSearchLocators.LOCATOR_LOGIN_FIELD[1]}")
         login_field = self.find_element(TestSearchLocators.LOCATOR_LOGIN_FIELD)
         login_field.clear()
         login_field.send_keys(word)
 
     def enter_pass(self, word):
+        logging.info(f"Send {word} to element {TestSearchLocators.LOCATOR_PASS_FIELD[1]}")
         login_field = self.find_element(TestSearchLocators.LOCATOR_PASS_FIELD)
         login_field.clear()
         login_field.send_keys(word)
 
     def click_login_button(self):
+        logging.info("Click login button")
         self.find_element(TestSearchLocators.LOCATOR_LOGIN_BTN).click()
         
     def get_error_text(self):
         error_field = self.find_element(TestSearchLocators.LOCATOR_ERROR_FIELD, time=3)
-        return error_field.text
+        text = error_field.text
+        logging.info(f"We find text {text} in error field {TestSearchLocators.LOCATOR_ERROR_FIELD[1]}")
+        return text
